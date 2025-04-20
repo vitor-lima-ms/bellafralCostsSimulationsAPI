7d88255da0c4a867b8bf9e01855c868d123b560c
Vitor Lima <viitorlima2000@gmail.com>
2025-04-20 20:09:09 -0300

    Update urls.py

--- cost/urls.py
+++ cost/urls.py
@@ -4,5 +4,5 @@ from cost import views
 urlpatterns = [
     path('', views.CostsAPIView.as_view(), name='costs'),
 
-    path('<int:pk>', views.CostAPIView.as_view(), name='cost'),
+    path('<int:pk>/', views.CostAPIView.as_view(), name='cost'),
 ]
