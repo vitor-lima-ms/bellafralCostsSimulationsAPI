b57cb7daa7629e1f215c471b86994df678734e05
Vitor Lima <viitorlima2000@gmail.com>
2025-04-20 20:09:11 -0300

    Update views.py

--- cost/views.py
+++ cost/views.py
@@ -1,13 +1,14 @@
-from rest_framework import status
 from rest_framework import generics
-from rest_framework.response import Response
+# from rest_framework import permissions
 
 from cost.models import Cost
 from cost.serializers import CostSerializer
+# from cost.permissions import IsSuperUser
 
 # Create your views here.
 
 class CostsAPIView(generics.ListCreateAPIView):
+    # permission_classes = (IsSuperUser, permissions.DjangoModelPermissions,) # Modifica o acesso, apenas usuários autenticados podem acessar por get
     queryset = Cost.objects.all()
     serializer_class = CostSerializer
 
